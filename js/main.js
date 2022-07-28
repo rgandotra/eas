23433d22944ad606f0925713a310a8848f954ba5
Raghav Gandotra <raghavgandotra16@gmail.com>
2022-07-28 14:18:11 -0400

    create addButtonClickListener(), refactor css to use variables for grid row and column

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-const numRows = 16;
-const numCols = 16;
-
-var gridContainer = document.getElementById("grid-container");
-
-for (i = 0; i < numRows; i++) {
-  for (j = 0; j < numCols; j++) {
-    var cell = document.createElement("div");
-    gridContainer.appendChild(cell);
-    cell.classList.add("grid-item");
+function createGrid(size) {
+  var gridContainer = document.getElementById("grid-container");
+  gridContainer.style.setProperty("--grid-rows", size);
+  gridContainer.style.setProperty("--grid-cols", size);
+  for (i = 0; i < size * size; i++) {
+    var gridItem = document.createElement("div");
+    gridContainer.appendChild(gridItem);
+    gridItem.classList.add("grid-item");
   }
+  //need to call mouseHoverListener every time a new grid is created
+  //because the child elements were deleted
+  addMouseHoverListeners();
 }
 
 function addMouseHoverListeners() {
@@ -20,4 +21,26 @@ function addMouseHoverListeners() {
   });
 }
 
-addMouseHoverListeners();
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function addButtonClickListener() {
+  const sizeSelectionButton = document.getElementById("size-selection-button");
+  sizeSelectionButton.addEventListener("click", () => {
+    const size = prompt("How many rows?", defaultSize);
+    if (size == null || size == "" || size > 100) {
+      alert("invalid input: Size must be between 1-100");
+    } else {
+      var gridContainer = document.getElementById("grid-container");
+      removeAllChildNodes(gridContainer);
+      createGrid(size);
+    }
+  });
+}
+
+const defaultSize = 16;
+createGrid(defaultSize);
+addButtonClickListener();
