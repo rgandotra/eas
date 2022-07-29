afada2bc8870f5eb0a767fc767b07756c97a292c
Raghav Gandotra <raghavgandotra16@gmail.com>
2022-07-28 23:28:48 -0400

    refactor

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function createGrid(size) {
     gridItem.classList.add("grid-item");
   }
   //need to call mouseHoverListener every time a new grid is created
-  //because the child elements were deleted
+  //because the child elements and respective event listeners are deleted by addButtonClickListener()
   addMouseHoverListeners();
 }
 
@@ -31,7 +31,9 @@ function addButtonClickListener() {
   const sizeSelectionButton = document.getElementById("size-selection-button");
   sizeSelectionButton.addEventListener("click", () => {
     const size = prompt("How many rows?", defaultSize);
-    if (size == null || size == "" || size > 100) {
+    const sizeNotSet = !size;
+    const sizeInRange = 0 < size && size <= 100;
+    if (sizeNotSet || !sizeInRange) {
       alert("invalid input: Size must be between 1-100");
     } else {
       var gridContainer = document.getElementById("grid-container");
